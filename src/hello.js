8ad3d9f413c0b03e83b6d47cf8ab4f320ffb85b2
Tom Enden <tome@wix.com>
2015-08-24 15:38:27 +0300

    exercise 6

--- src/hello.js
+++ src/hello.js
@@ -1,22 +1,34 @@
 define(['react', 'lodash', './hello.rt'], function (React, _, template) {
     'use strict';
 
+    var countries = {
+        Israel: [
+            'Tel Aviv',
+            'Haifa',
+            'Raanana'
+        ],
+        'United States': [
+            'Manhattan',
+            'Boston'
+        ]
+    };
+
     return React.createClass({
         mixins: [React.addons.LinkedStateMixin],
+        getCountries: function () {
+            return _.keys(countries);
+        },
+        getCities: function () {
+            var selectedCountry = this.state.selectedCountry;
+            return selectedCountry ?
+                countries[selectedCountry] :
+                '';
+        },
         getInitialState: function () {
             return {
-                fields: [],
-                newField: ''
+                selectedCountry: null,
+                selectedCity: null
             };
-        },
-        addField: function () {
-            var newState = _.cloneDeep(this.state);
-            newState.fields.push(this.state.newField);
-            newState.newField = '';
-            this.setState(newState);
-        },
-        logChange: function (event) {
-            window.console.log(event.target.value);
         },
         displayName: 'Hello',
         render: template
