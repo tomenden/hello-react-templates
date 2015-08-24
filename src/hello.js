e481a822d4ec95fdd269fe2522cef5d3571c4e83
Tom Enden <tome@wix.com>
2015-08-24 17:13:10 +0300

    exercise 10

--- src/hello.js
+++ src/hello.js
@@ -1,23 +1,29 @@
 define(['react', 'lodash', './hello.rt'], function (React, _, template) {
     'use strict';
 
+    var classes = ['large', 'small', 'huge', 'x-small', 'italic', 'bold'];
 
     return React.createClass({
         mixins: [React.addons.LinkedStateMixin],
         getInitialState: function () {
             return {
-                height: 50,
-                padding: 50
+                currentClassIndex: 0
             };
         },
-        getProps: function () {
-            return {
-                style: {
-                    backgroundColor: 'pink',
-                    height: this.state.height + 'px',
-                    padding: this.state.padding + 'px 0'
-                }
-            };
+        componentDidUpdate: function () {
+            this.refs.text.getDOMNode().className = classes[this.state.currentClassIndex];
+        },
+        lowerClass: function () {
+            var newClassIndex = this.state.currentClassIndex === 0 ?
+            classes.length - 1 :
+            this.state.currentClassIndex - 1;
+            this.setState({currentClassIndex: newClassIndex});
+        },
+        higherClass: function () {
+            var newClassIndex = this.state.currentClassIndex === classes.length - 1 ?
+                0 :
+            this.state.currentClassIndex + 1;
+            this.setState({currentClassIndex: newClassIndex});
         },
         displayName: 'Hello',
         render: template
