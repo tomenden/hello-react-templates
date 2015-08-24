e4e37a80883d1f7026de5c33edac6ff6fcb51deb
Tom Enden <tome@wix.com>
2015-08-24 14:58:43 +0300

    exercise number 4

--- src/hello.js
+++ src/hello.js
@@ -2,6 +2,26 @@ define(['react', 'lodash', './hello.rt'], function (React, _, template) {
     'use strict';
 
     return React.createClass({
+        mixins: [React.addons.LinkedStateMixin],
+        getInitialState: function () {
+            return {
+                location: {
+                    long: 0,
+                    lat: 1
+                }
+            };
+        },
+        handleChange: function (locationToChange, newValue) {
+            var newState = _.cloneDeep(this.state);
+            newState.location[locationToChange] = newValue;
+            this.setState(newState);
+        },
+        getValueLink: function(type) {
+            return {
+                value: this.state.location[type],
+                requestChange: this.handleChange.bind(this, type)
+            };
+        },
         displayName: 'Hello',
         render: template
     });
